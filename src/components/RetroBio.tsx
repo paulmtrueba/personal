5084e32048b90dbf84acc505adda7cf0ae21af47
Paul Trueba <pault.tripwirearts@gmail.com>
2026-06-16 16:27:59 -0700

    Update RetroBio.tsx

--- src/components/RetroBio.tsx
+++ src/components/RetroBio.tsx
@@ -32,7 +32,7 @@ export function RetroBio() {
               <div className="flex justify-between items-center">
                 <span>Welcome to My Homepage</span>
                 <span className="text-xs font-normal">
-                  [ Last Updated: 05/24/2026 ]
+                  [ Last Updated: 06/16/2026 ]
                 </span>
               </div>
             </td>
