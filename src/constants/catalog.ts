84f345fda046ebdea7937c31aa48dc81ee87ac9e
Paul Trueba <pault.tripwirearts@gmail.com>
2026-06-16 16:24:10 -0700

    Update catalog.ts

--- src/constants/catalog.ts
+++ src/constants/catalog.ts
@@ -39,4 +39,9 @@ export const catalog = [
 		publication: "Submitted Short Fiction",
 		year: 2026,
 	},
+	{
+		title: "The Intermission",
+		publication: "Writing Short Fiction",
+		year: 2026,
+	},
 ];
